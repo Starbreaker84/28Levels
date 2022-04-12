a99e8a8036556a1d058275639bc0f3d5880a7d30
Ruslan Omalaev <romalaev@gmail.com>
2022-04-12 20:41:27 +0300

    Add Level1-6.java

---
+++ Level1-6.java
@@ -0,0 +1,24 @@
+public class Level1 {
+    public static String PatternUnlock(int N, int [] hits) {
+        String code = "";
+        if (N == 1) {
+            code = Integer.toString(hits[0]);
+            return code;
+        }
+        double line = 0;
+        for (int i = 1; i < N; i++) {
+            int checker = hits[i] + hits[i - 1];
+            if ((checker == 6 || checker == 8) || ((checker == 9 || checker == 11) && (hits[i] >= 7 || hits[i - 1] >= 7))) {
+                line += Math.sqrt(2);
+            } else line += 1;
+        }
+        String str = String.format("%.5f", line);
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) != '0' && str.charAt(i) != '.') {
+                code = code + str.charAt(i);
+            }
+        }
+        return code;
+
+    }
+}
