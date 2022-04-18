aedb5631178b25f4333dfc1b025a87acd47521e7
Ruslan Omalaev <romalaev@gmail.com>
2022-04-18 16:22:27 +0300

    Update Level1-14.java

--- Level1-14.java
+++ Level1-14.java
@@ -3,6 +3,7 @@ public class Level1 {
         int time = 0;
         int distance = 0;
         for (int i = 0; i < N; i++) {
+            if (track[i][0] >= L) continue;
             time += track[i][0] - distance;
             distance += track[i][0] - distance;
             int loop = track[i][1] + track[i][2];
